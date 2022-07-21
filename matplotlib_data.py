30007a92637b6b7b4df0a4359e7d671933a0c3bf
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-07-21 20:43:47 +0530

    Add files via upload

---
+++ matplotlib_data.py
@@ -0,0 +1,81 @@
+# import matplotlib.pyplot as plt
+# x=[12,15,16,48]
+# y=[1,56,60,62]
+# plt.bar(x,y,color="yellow")
+# plt.show()
+
+# a=[12,15,20,35]
+# b=[5,8,19,25]
+# plt.bar(a,b,color="blue")
+# plt.show()
+
+# import numpy as np
+# print(np.__version__)
+# import matplotlib as mp
+# print(mp.__version__)
+
+# import numpy as np
+# a=np.array(55)
+# b=np.array([1,2,3,4])
+# c=np.array([[1,2,3],[4,5,6]])
+# d=np.array([[[1,2,3],[4,5,6]],[[1,2,3],[4,5,6]]])
+# print(a.ndim)
+# print(b.ndim)
+# print(c.ndim)
+# print(d.ndim)
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+# x=np.array([0,6])
+# y=np.array([5,10])
+# plt.plot(x,y)
+# plt.show() 
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+# x=np.array([1,8])
+# y=np.array([3,10])
+# plt.plot(x,y,'o')
+# plt.plot(x,y,'s')
+# plt.plot(x,y,'p')
+# plt.plot(x,y,'h')
+# plt.plot(x,y)
+# plt.show()
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+# x=np.array([1,2,6,8])
+# y=np.array([3,8,1,10])
+# plt.plot(x,y)
+# plt.plot(x,y,'p')
+# plt.plot(x,y,marker='p')
+# plt.plot(x,y,'s:k')
+# plt.plot(x,y,'s:c')
+# plt.plot(x,y,'p:y')
+# plt.plot(x,y,'s:w')
+# plt.plot(x,y,'s-b')
+# plt.plot(x,y,'s--r')
+# plt.plot(x,y,'s--g')
+# plt.plot(x,y,'s-.m')
+# plt.plot(x,y,'s-.g',ms=10,mec='r',mfc='k')
+# plt.plot(x,y,ls='dotted')
+# plt.plot(x,y,ls='dashdot')
+# plt.plot(x,y,ls='dashed')
+# plt.plot(x,y,ls='None')
+# plt.plot(x,y,ls='solid',lw='5')
+# plt.plot(x,y,ls='solid',lw='2')
+# plt.show()
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+# x=np.array([1,2,3,4,5])
+# y=np.array([10,20,30,40,50])
+# abc={'family':'TimesNewRoman','color':'blue','size':60}
+# font2={'family':'Harrington','color':'red','size':30}
+# plt.title("Course",fontdict=abc)
+# plt.xlabel("Python",fontdict=font1)
+# plt.ylabel(" adv Python",fontdict=font1)
+# plt.plot(x,y)
+# plt.show()
+# plt.pie(x)
+# plt.show()
